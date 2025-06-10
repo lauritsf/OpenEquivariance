a82d093a4d3369a6c2cbcbd270b253f7b9934960
asglover <140220574+asglover@users.noreply.github.com>
2025-06-09 18:22:44 -0700

    Pypi release (#124)
    * for testing "build"
    
    * added metadata
    
    * add dynamic versioning
    
    * add workflow for test.pypi uploads
    
    * add test that metadata and __version__ are the same
    
    * record minimum torch version
    
    * rename to release.yaml, change to pypi vs test.pypi
    
    * --sdist only
    
    ---------
    
    Co-authored-by: Austin Glover <austin_glover@berkeley.com>

--- tests/import_test.py
+++ tests/import_test.py
@@ -1,8 +1,12 @@
+from importlib.metadata import version
+
+
 def test_import():
     import openequivariance
 
     assert openequivariance.__version__ is not None
     assert openequivariance.__version__ != "0.0.0"
+    assert openequivariance.__version__ == version("openequivariance")
 
 
 def test_tutorial():
